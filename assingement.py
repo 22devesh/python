dac5881e5a96514b802c1244eae282bcc4ed5568
22devesh <115339771+22devesh@users.noreply.github.com>
2022-10-16 11:48:18 +0530

    Add files via upload

---
+++ assingement.py
@@ -0,0 +1,136 @@
+# Question For a = 60 , b = 13 compare the numbers using bitwise operators like AND, OR, XOR, NOT.
+
+a=60 # decimal value is 111100
+b=13 #decimal value is 1101
+
+# & = 001100
+# | = OR = 111101
+# ^ = XOR = 110001
+# - = NOT , "a"= 000011,"b"= 0010
+
+
+# QUESTION 01.  Which of the following statements assigns the value 25 to the variable x in Python:
+# (a)x ← 25
+# (b)x = 25
+# (c) x := 25
+# (d) int x = 25
+# (e) x << 25
+
+# ans is (b)
+
+ 
+# 02. In Python, a variable may be assigned a value of one type, and then later assigned a value of a different type:
+# False
+# True
+
+#ans is true because it can be convereted in another type also
+
+# 03. Which one of the following is the correct way of declaring and initializing a variable, x with the value 7?
+# int x
+# x=7
+# int x=7
+# x=7
+# declare x=7
+ 
+ #ans is - x= 7
+
+ 
+ 
+# 04. What will be the output of statement 2**2**2**2
+# 16
+# 256
+# 32768
+# 65536
+
+print(2**2**2**2) # = 65536
+
+
+# 05. Which of the following statement is False?
+# Variable names can be arbitrarily long.
+# They can contain both letters and numbers.
+# Variable name can begin with underscore.
+# Variable name can begin with number.
+
+# ans is - variable name can begin with number
+
+
+# 06. What is the output of the following code: print 9//2
+# 4
+# 4.5
+# 4.0
+# Error
+print(9//2) # = 4
+
+# 07. Which of the following is not a valid variable name in Python?
+# _var
+# var_name
+# var11
+# 5var
+
+# ans is - 5var
+ 
+ 
+# 08. What is the maximum length of an identifier in python?
+# 32
+# 31
+# 63
+# None of the above
+
+# ans is = none of the above
+
+
+# 09. Which of the following declarations is incorrect?
+# None Of the below
+# _x = 2
+# __x = 3
+# __xyz__ = 5
+
+# ans is - none of the below 
+
+
+# 10. What is the result of round(0.5) – round(-0.5)?
+# 1.0
+# 2.0
+# 0
+# None Of the above
+
+# ans is - 1.0
+
+
+# 11. In Python, a variable must be declared before it is assigned a value:
+# True
+# False
+ 
+# ans is - true 
+ 
+# 12. Why does the name of local variables start with an underscore discouraged?
+# To identify the variable
+# It confuses the interpreter
+# It indicates a private variable of a class
+# None of these
+ 
+# ans is - none of the above
+
+# 13. Which of the following will run without errors?
+# round(75.8)
+# round()
+# round(5352.898,9,5)
+# round(6463.123,2,6)
+
+# ans is - round(75.8)
+
+# 14. Which of the following is a valid variable?
+# var@
+# 32var
+# in
+# abc_x
+
+#ans is - abc_x
+
+# 15. Is Python case sensitive when dealing with identifiers?
+# Yes
+# No
+# Machine dependent
+#None of the above
+ 
+ # ans is - yes
